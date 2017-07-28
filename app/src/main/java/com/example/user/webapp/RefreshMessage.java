12411a564dad000ca8cc975f37d27ec6bba9b07e
maydaychen <cy245832451@outlook.com>
2017-07-28 18:08:17 +0800

    7-28

---
+++ app/src/main/java/com/example/user/webapp/RefreshMessage.java
@@ -0,0 +1,15 @@
+package com.example.user.webapp;
+
+/**
+ * Created by user on 2017/7/28.
+ */
+
+public class RefreshMessage {
+    public int refresh;
+
+    public RefreshMessage(int refresh) {
+        //1,session
+        //2,access
+        this.refresh = refresh;
+    }
+}
---
+++ app/src/main/java/com/example/user/webapp/http/HttpMethods.java
@@ -0,0 +1,64 @@
+package com.example.user.webapp.http;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.functions.Func1;
+
+/**
+ * 作者：JTR on 2016/11/25 10:18
+ * 邮箱：2091320109@qq.com
+ */
+public class HttpMethods {
+    public static final String BASE_URL = "https://api.duoyunjiav2.wshoto.com";
+    private static final int DEFAULT_TIMEOUT = 5;
+
+    private Retrofit retrofit;
+    private BlueService movieService;
+
+    private HttpMethods() {
+        //手动创建一个OkHttpClient并设置超时时间
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+        httpClientBuilder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+
+        retrofit = new Retrofit.Builder()
+                .client(httpClientBuilder.build())
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(BASE_URL)
+                .build();
+
+        movieService = retrofit.create(BlueService.class);
+    }
+
+    private static class SingletonHolder {
+        private static final HttpMethods INSTANCE = new HttpMethods();
+    }
+
+    //获取单例
+    public static HttpMethods getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private class HttpResultFunc<T> implements Func1<HttpResult<T>, T> {
+        @Override
+        public T call(HttpResult<T> httpResult) {
+            return httpResult.getOthers();
+        }
+    }
+
+    //
+
+//    public void get_token(Subscriber<JSONObject> subscriber, String apiname, String apipass) {
+//        movieService.index_info(apiname,apipass)
+////                .map(new HttpResultFunc<>())
+//                .subscribeOn(Schedulers.io())
+//                .unsubscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(subscriber);
+//    }
+
+}
